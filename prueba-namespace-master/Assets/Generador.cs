9b3888a070b4c1da898147bba8eab9be7edc06a0
Kazui97 <camyescobar12@gmail.com>
2019-10-04 14:21:22 -0500

    Initial commit

---
+++ prueba-namespace-master/Assets/Generador.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NPC.enemy;
+using NPC.Ally;
+using System;
+
+
+
+public class Generador : MonoBehaviour
+{
+    GameObject ZombieMesh;
+    GameObject Gente;
+    GameObject Hero;
+    CosasZombie datoZombi;
+    CosasCiudadanos datoCiudadanos;
+    readonly int minimo;
+    const int maximo = 25;
+    int cantbody;
+    public Text enemy;
+    public Text ally;
+
+
+
+    System.Random rn = new System.Random();
+
+    public Generador()
+    {
+        minimo = rn.Next(5, 15);    //rango de creación
+        
+    }
+
+    void Start()
+    {                                 // generador de NPC
+        cantbody = rn.Next(minimo, maximo);
+        for (int i = 0; i < cantbody; i++)
+        {
+            if (rn.Next(0,2)==0)
+            {                               // generador de zombis
+                ZombieMesh = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                ZombieMesh.AddComponent<ZombieOp>();
+
+                datoZombi = ZombieMesh.GetComponent<ZombieOp>().datosZombi;
+                switch (datoZombi.colorEs)
+                {
+                    case CosasZombie.ColorZombie.magenta:
+                        ZombieMesh.GetComponent<Renderer>().material.color = Color.magenta;
+
+                        break;
+                    case CosasZombie.ColorZombie.green:
+                        ZombieMesh.GetComponent<Renderer>().material.color = Color.green;
+
+                        break;
+                    case CosasZombie.ColorZombie.cyan:
+                        ZombieMesh.GetComponent<Renderer>().material.color = Color.cyan;
+                        break;
+                }
+
+
+                Vector3 pos = new Vector3(rn.Next(-10, 10), 0, rn.Next(-10, 10));
+                ZombieMesh.transform.position = pos;
+                ZombieMesh.AddComponent<Rigidbody>();
+                ZombieMesh.name = "Zombi";
+            }
+            else // generador de ciudadanos
+            {
+                Gente = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Gente.AddComponent<CiudadanoOp>();
+                Vector3 po = new Vector3(rn.Next(-20, 10), 0, rn.Next(10, 10));
+                Gente.transform.position = po;
+                Gente.AddComponent<Rigidbody>();
+                Gente.name = "Gente";
+            }
+        }
+       
+        // generador hero 
+        Hero = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Hero.AddComponent<MovimientoTeclado>();
+        Hero.AddComponent<Hero>();
+        Hero.AddComponent<Camera>(); 
+        Hero.AddComponent<Rigidbody>();
+        Hero.name = "Hero";
+
+            
+       int numzombie = 0;
+       int numaldeanos = 0;
+
+       
+        foreach (ZombieOp enemy in Transform.FindObjectsOfType<ZombieOp>())
+        {
+           numzombie++;
+        }
+
+        foreach (CiudadanoOp ally in Transform.FindObjectsOfType<CiudadanoOp>())
+        {
+            numaldeanos++;
+        }
+        Debug.Log(numzombie);
+        ally.text="aldeanos: "+numaldeanos;
+        enemy.text="zombies: "+numzombie;
+      
+    }   
+      
+    
+
+
+}
